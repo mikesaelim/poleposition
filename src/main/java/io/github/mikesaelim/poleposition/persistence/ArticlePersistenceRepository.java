e9ebbcf7a77e7a60fad3b1adf58856d4de273873
Mike Saelim <mike.saelim@gmail.com>
2016-07-19 03:16:11 -0500

    Make IntelliJ stop crying by adding unnecessary @Repository annotation

--- src/main/java/io/github/mikesaelim/poleposition/persistence/ArticlePersistenceRepository.java
+++ src/main/java/io/github/mikesaelim/poleposition/persistence/ArticlePersistenceRepository.java
@@ -1,6 +1,8 @@
 package io.github.mikesaelim.poleposition.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticlePersistenceRepository extends JpaRepository<ArticlePersistence, String> {
 }
