82d9538da3d4f719c2b328a8da26db1029fa19e2
Mike Saelim <mike.saelim@gmail.com>
2016-07-19 04:47:02 -0500

    Wire up the repository to the ingestion service

--- src/main/java/io/github/mikesaelim/poleposition/service/ArxivIngestionService.java
+++ src/main/java/io/github/mikesaelim/poleposition/service/ArxivIngestionService.java
@@ -3,6 +3,9 @@ package io.github.mikesaelim.poleposition.service;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
 
+/**
+ * TODO javadoc
+ */
 public interface ArxivIngestionService {
 
     void ingestRecord(String identifier) throws URISyntaxException;
--- src/main/java/io/github/mikesaelim/poleposition/service/ArxivIngestionServiceImpl.java
+++ src/main/java/io/github/mikesaelim/poleposition/service/ArxivIngestionServiceImpl.java
@@ -5,6 +5,7 @@ import io.github.mikesaelim.arxivoaiharvester.model.request.GetRecordRequest;
 import io.github.mikesaelim.arxivoaiharvester.model.request.ListRecordsRequest;
 import io.github.mikesaelim.arxivoaiharvester.model.response.GetRecordResponse;
 import io.github.mikesaelim.arxivoaiharvester.model.response.ListRecordsResponse;
+import io.github.mikesaelim.poleposition.persistence.ArticlePersistenceRepository;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,20 +13,31 @@ import org.springframework.stereotype.Service;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
 
+import static io.github.mikesaelim.poleposition.service.ArticleMapper.toPersistence;
+
+/**
+ * TODO javadoc, exception handling
+ */
 @Service
 public class ArxivIngestionServiceImpl implements ArxivIngestionService {
 
     @Autowired
     private ArxivOAIHarvester harvester;
 
+    @Autowired
+    private ArticlePersistenceRepository repository;
+
     @Override
     public void ingestRecord(@NonNull String identifier) throws URISyntaxException {
         GetRecordRequest request = new GetRecordRequest(identifier);
         GetRecordResponse response = harvester.harvest(request);
 
-        // TODO make this persist to MySQL
-        System.out.println("Returned record:");
-        System.out.println(response.getRecord().toString());
+        if (response.getRecord() != null) {
+            repository.save(toPersistence(response.getRecord()));
+            System.out.println("Record for identifier = " + identifier + " saved.");
+        } else {
+            System.out.println("Record for identifier = " + identifier + " not found.");
+        }
     }
 
     @Override
@@ -34,8 +46,11 @@ public class ArxivIngestionServiceImpl implements ArxivIngestionService {
         while (request != ListRecordsRequest.NONE) {
             ListRecordsResponse response = harvester.harvest(request);
 
-            // TODO make this persist to MySQL
-            System.out.println("Returned page of size " + response.getRecords().size());
+            response.getRecords().stream()
+                    .map(ArticleMapper::toPersistence)
+                    .forEach(repository::save);
+
+            System.out.println("Saved " + response.getRecords().size() + " records.");
 
             request = response.resumption();
         }
